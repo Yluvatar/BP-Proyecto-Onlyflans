919f184d20768a4792b07d86d5eddb7af0fd3c74
Juan Pablo Scheihing <eru.yluvatar@gmail.com>
2024-06-06 19:33:54 -0400

    Agregando configuración para subir a Heroku 2

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
 
 
